43933d883f9445c942a04ffc3fcbd345fa607f25
DustinMiner <dustinjminer@outlook.com>
2019-03-05 20:46:18 -0600

    Add Routes and static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # User sneds web request to /hello_world and the routes file sends it to the hello action in the static_pages controler
   get '/hello_world' => 'static_pages#hello'
+  get '/' => 'static_pages#home'
+  get '/home' => 'static_pages#home'
+
 end
